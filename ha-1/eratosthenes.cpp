9891de593737d757b524665416b5c6871d663423
Nepomuk Frädrich <m@nepda.eu>
2012-05-05 19:37:43 +0200

    count multi strikes

--- ha-1/eratosthenes.cpp
+++ ha-1/eratosthenes.cpp
@@ -71,13 +71,16 @@ void markMultiples(int y, int M, int * marked, int &mark_count)
     {
 	printf("x%i", mark_count);
 	mark_count = mark_count + 1;
-	marked[c] = 1;
+	if (marked[c] == -1)
+	    marked[c] = 1;
+	else
+	    marked[c] = marked[c] + 1;
     }
 }
 
 int main ()
 {
-    eratosthenes(2315);
+    eratosthenes(76);
     return 0;
 }
 
